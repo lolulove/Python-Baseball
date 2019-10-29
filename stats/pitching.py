c972787be7da799fc626d11a116be522435c35ee
lolulove <32493664+lolulove@users.noreply.github.com>
2019-10-29 22:25:04 +0100

    Completed the project

--- stats/pitching.py
+++ stats/pitching.py
@@ -7,10 +7,8 @@ plays = games[games['type'] == 'play']
 strike_outs = plays[plays['event'].str.contains('K')]
 strike_outs = strike_outs.groupby(['year', 'game_id']).size()
 strike_outs = strike_outs.reset_index(name='strike_outs')
-strike_outs = strike_outs.loc[:, ['year', 'strike_outs']
-].apply(pd.to_numeric)
+strike_outs = strike_outs.loc[:, ['year', 'strike_outs']].apply(pd.to_numeric)
 
-strike_outs.plot(x='year', y='strike_outs', kind='scatter'
-).lengend(['Strike Outs'])
+strike_outs.plot(x='year', y='strike_outs', kind='scatter').legend(['Strike Outs'])
 
 plt.show()
