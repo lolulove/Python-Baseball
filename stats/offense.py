d27774130a048b236d15f228fd65e303e7758ce6
lolulove <32493664+lolulove@users.noreply.github.com>
2019-10-31 18:05:24 +0100

    Completed the project

--- stats/offense.py
+++ stats/offense.py
@@ -1,2 +1,25 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+
+from data import games
+
+plays = games[games['type'] == 'play']
+plays.columns = ['type', 'inning', 'team', 'player', 'count', 'pitches',
+'event', 'game_id', 'year']
+
+hits = plays.loc[plays['event'].str.contains('^(?:S(?!B)|D|T|HR)'),
+ ['inning', 'events']]
+
+hits.loc[:, 'inning'] = pd.to_numeric(hits.loc[:, 'inning'])
+
+replacements = { r'^S(.*)': 'single', r'^D(.*)': 'double', r'^T(.*)': 'triple',
+r'^HR(.*)': 'hr' }
+
+hit_type = hits['event'].replace(replacements, regex=True)
+
+hits = hits.assign(hit_type=hit_type)
+
+hits = hits.groupby(['inning', 'hit_type']).size().reset_index(name='count')
+
+hits['hit_type'] = pd.Categorical(hits['hit_type'], ['single', 'double',
+'triple', 'hr'])
