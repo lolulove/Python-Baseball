1ea05e58a96585f800a4492d862092313a04f262
lolulove <32493664+lolulove@users.noreply.github.com>
2019-10-31 18:16:23 +0100

    Completed the project

--- stats/offense.py
+++ stats/offense.py
@@ -4,11 +4,10 @@ import matplotlib.pyplot as plt
 from data import games
 
 plays = games[games['type'] == 'play']
-plays.columns = ['type', 'inning', 'team', 'player', 'count', 'pitches',
-'event', 'game_id', 'year']
+plays.columns = ['type', 'inning', 'team', 'player', 'count', 'pitches', 'event', 'game_id', 'year']
 
 hits = plays.loc[plays['event'].str.contains('^(?:S(?!B)|D|T|HR)'),
- ['inning', 'events']]
+ ['inning', 'event']]
 
 hits.loc[:, 'inning'] = pd.to_numeric(hits.loc[:, 'inning'])
 
